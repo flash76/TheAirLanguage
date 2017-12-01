cee1868be8f9d88712f7e6af602c97137464e97e
flash76 <flash76@users.noreply.github.com>
2017-12-01 10:28:08 -0500

    Update printer.java

--- printer.java
+++ printer.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class printer {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a string");
+        s = in .nextLine();
+        System.out.println("You entered string " + s);
+
+        System.out.println("Enter an integer");
+        a = in .nextInt();
+        System.out.println("You entered integer " + a);
+
+        System.out.println("Enter a float");
+        b = in .nextFloat();
+        System.out.println("You entered float " + b);
+    }
+}
