9a29158834e0bd72fd29034cdacab918478c2a22
flash76 <flash76@users.noreply.github.com>
2017-12-01 10:35:11 -0500

    Update compiler.java

--- compiler.java
+++ compiler.java
@@ -1,5 +1,17 @@
-public class HelloWorld
+import java.util.Scanner;
+
+public class compiler
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		try 
+		{
+			String[] splitInp = input.split("\\s+");
+			System.out.println(splitInp[1]);
+		} 
+		catch (PatternSyntaxException ex) {
+			// 
+		}
 	}
 }
